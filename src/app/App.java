06e095dfef8572ed4cefef377198dc1c654e1bb5
Yousef Razzouk <yousefrazouk888@gmail.com>
2025-12-06 15:23:27 +0300

    ignoring antlr package

--- src/app/App.java
+++ src/app/App.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 // CRITICAL IMPORT: Import the custom Lexer class
 import antlr.JinjaFlaskIndentingLexer;
-import antlr.JinjaFlaskParser;
 
 public class App {
     public static void main(String[] args) {
